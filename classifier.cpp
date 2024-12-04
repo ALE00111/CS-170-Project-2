04ed1a4205666c34bb8c5560a5941d14f41ea5f8
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2024-12-03 17:00:27 -0800

    classifier.h and.cpp

--- classifier.cpp
+++ classifier.cpp
@@ -1,3 +1,38 @@
 #include "classifier.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
+using namespace std;
 
+
+    // Train function to input training instances
+    void Classifier::Train(const vector<Instance>& trainingData) {
+        trainingInstances = trainingData; //Assigns data to member variable trainingInstances
+    }
+
+    // Test function to predict the class label for instance
+    int Classifier::Test(const Instance& testInstance) {
+        double minDistance = numeric_limits<double>::max(); //assign largest double value to min distance
+        int predictedLabel = -1;
+
+        for (const auto& instance : trainingInstances) {
+            double distance = ComputeDistance(testInstance.features, instance.features);
+            if (distance < minDistance) {
+                minDistance = distance;
+                predictedLabel = instance.classLabel;
+            }
+        }
+
+        return predictedLabel;
+    }
+
+    // Helper function to compute Euclidean distance between two feature vectors
+    double Classifier::ComputeDistance(const vector<double>& features1, const vector<double>& features2) {
+        double sum = 0.0;
+        for (size_t i = 0; i < features1.size(); ++i) {
+            sum += pow(features1[i] - features2[i], 2);
+        }
+        return sqrt(sum);
+    }
 
--- classifier.h
+++ classifier.h
@@ -1,16 +1,27 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
+
 using namespace std; 
 
-
+// Define a structure for storing instances
+struct Instance {
+    vector<double> features;
+    int classLabel;
+};
 //This class is used to hold training instances in memory and use NN
 //Use Euclidean distance to measure the distance between two points
 
 class Classifier {
     public:
         Classifier();
-        void Train(); //function for inputting training instances
-        int Test(); //function for inputting test instances and output is the prdicted class label
+        void Train(const vector<Instance>& trainingData); //function for inputting training instances
+        int Test(const Instance& testInstance); //function for inputting test instances and output is the prdicted class label
     private:
-
+        vector<Instance> trainingInstances;
+        double ComputeDistance(const vector<double>& features1, const vector<double>& features2);
 };
-
+#endif
