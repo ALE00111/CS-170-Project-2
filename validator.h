6c9e0c2cdaba25410a567a1c56a9127bb033643b
apermatigari <apermatigari@gmail.com>
2024-12-05 00:17:30 -0800

    Validator rough draft

--- validator.h
+++ validator.h
@@ -6,7 +6,6 @@
 #include <iostream>
 using namespace std;
 
-
 //This class is for the evaluation module that returns the accuract of the classifier
 //of a given dataset, when using the given features subset
 class Validator {
